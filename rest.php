caddc7b05c05f2be6a16aa3ca6da6c4efd1917ea
talentd1223 <talentd1223@gmail.com>
2022-11-15 12:37:58 +0900

    update

--- rest.php
+++ rest.php
@@ -30,44 +30,50 @@ if ($uri = "data" && $method = "POST"){
 
     $connection = db_connect();
 
-    print_r(isset($_POST['ship_from_city']) ? $_POST['ship_from_city'] : "");
+    // print_r(isset($data['ship_from_city']) ? $data['ship_from_city'] : "");
 
-    $ship_from_city = isset($_POST['ship_from_city']) ? $_POST['ship_from_city'] : "";
-    
-    $sql = "INSERT INTO data (ship_from_name, ship_from_address1, ship_from_address2, ship_from_city, ship_from_state, ship_from_zip, ship_to_name, ship_to_address1, ship_to_address2, ship_to_city, ship_to_state, ship_to_zip, return_address_name, return_address_address1, return_address_address2, return_address_city, return_address_state, return_address_zip, flat_rate, services, weight_lbs, weight_oz, dimensions) VALUES ('" . (isset($_POST['ship_from_name']) ? $_POST['ship_from_name'] : "")
-    . "','" . (isset($_POST['ship_from_address1']) ? $_POST['ship_from_address1'] : "")
-    . "','" . (isset($_POST['ship_from_address2']) ? $_POST['ship_from_address2'] : "")
-    . "','" . (isset($_POST['ship_from_city']) ? $_POST['ship_from_city'] : "")
-    . "','" . (isset($_POST['ship_from_state']) ? $_POST['ship_from_state'] : "") 
-    . "','" . (isset($_POST['ship_from_zip']) ? $_POST['ship_from_zip'] : "")
-    . "','" . (isset($_POST['ship_to_name']) ? $_POST['ship_to_name'] : "")
-    . "','" . (isset($_POST['ship_to_address1']) ? $_POST['ship_to_address1'] : "") 
-    . "','" . (isset($_POST['ship_to_address2']) ? $_POST['ship_to_address2'] : "")
-    . "','" . (isset($_POST['ship_to_city']) ? $_POST['ship_to_city'] : "")
-    . "','" . (isset($_POST['ship_to_state']) ? $_POST['ship_to_state'] : "")
-    . "','" . (isset($_POST['ship_to_zip']) ? $_POST['ship_to_zip'] : "")
-    . "','" . (isset($_POST['return_address_name']) ? $_POST['return_address_name'] : "")
-    . "','" . (isset($_POST['return_address_address1']) ? $_POST['return_address_address1'] : "")
-    . "','" . (isset($_POST['return_address_address2']) ? $_POST['return_address_address2'] : "")
-    . "','" . (isset($_POST['return_address_city']) ? $_POST['return_address_city'] : "")
-    . "','" . (isset($_POST['return_address_state']) ? $_POST['return_address_state'] : "")
-    . "','" . (isset($_POST['return_address_zip']) ? $_POST['return_address_zip'] : "")
-    . "','" . (isset($_POST['flat_rate']) ? $_POST['flat_rate'] : "")
-    . "','" . (isset($_POST['service']) ? $_POST['service'] : "")
-    . "','" . (isset($_POST['weight_lbs']) ? $_POST['weight_lbs'] : "")
-    . "','" . (isset($_POST['weight_oz']) ? $_POST['weight_oz'] : "")
-    . "','" . (isset($_POST['dimensions']) ? $_POST['dimensions'] : "")
+    // $ship_from_city = isset($data['ship_from_city']) ? $data['ship_from_city'] : "";
+
+    $params = file_get_contents('php://input');
+    $data = json_decode($params, true);
+
+    $sql = "INSERT INTO data (ship_from_name, ship_from_address1, ship_from_address2, ship_from_city, ship_from_state, ship_from_zip, ship_to_name, ship_to_address1, ship_to_address2, ship_to_city, ship_to_state, ship_to_zip, return_address_name, return_address_address1, return_address_address2, return_address_city, return_address_state, return_address_zip, flat_rate, services, weight_ibs, weight_oz, dimensions) VALUES ('" . (isset($data['ship_from_name']) ? $data['ship_from_name'] : "")
+    . "','" . (isset($data['ship_from_address1']) ? $data['ship_from_address1'] : "")
+    . "','" . (isset($data['ship_from_address2']) ? $data['ship_from_address2'] : "")
+    . "','" . (isset($data['ship_from_city']) ? $data['ship_from_city'] : "")
+    . "','" . (isset($data['ship_from_state']) ? $data['ship_from_state'] : "") 
+    . "','" . (isset($data['ship_from_zip']) ? $data['ship_from_zip'] : "")
+    . "','" . (isset($data['ship_to_name']) ? $data['ship_to_name'] : "")
+    . "','" . (isset($data['ship_to_address1']) ? $data['ship_to_address1'] : "") 
+    . "','" . (isset($data['ship_to_address2']) ? $data['ship_to_address2'] : "")
+    . "','" . (isset($data['ship_to_city']) ? $data['ship_to_city'] : "")
+    . "','" . (isset($data['ship_to_state']) ? $data['ship_to_state'] : "")
+    . "','" . (isset($data['ship_to_zip']) ? $data['ship_to_zip'] : "")
+    . "','" . (isset($data['return_address_name']) ? $data['return_address_name'] : "")
+    . "','" . (isset($data['return_address_address1']) ? $data['return_address_address1'] : "")
+    . "','" . (isset($data['return_address_address2']) ? $data['return_address_address2'] : "")
+    . "','" . (isset($data['return_address_city']) ? $data['return_address_city'] : "")
+    . "','" . (isset($data['return_address_state']) ? $data['return_address_state'] : "")
+    . "','" . (isset($data['return_address_zip']) ? $data['return_address_zip'] : "")
+    . "','" . (isset($data['flat_rate']) ? $data['flat_rate'] : "")
+    . "','" . (isset($data['service']) ? $data['service'] : "")
+    . "','" . (isset($data['weight_ibs']) ? $data['weight_ibs'] : "")
+    . "','" . (isset($data['weight_oz']) ? $data['weight_oz'] : "")
+    . "','" . (isset($data['dimensions']) ? $data['dimensions'] : "")
     . "')";
-
-    echo $sql;
+    // echo $sql;
 
 }
     
-
 if ($connection->query($sql) === TRUE) {
-    echo "New record created successfully";
+    $data = array('status' => true, 'message' => "Data saved successfully!");
+
+    echo json_encode($data);
+
   } else {
-    echo "Error: " . $sql . "<br>" . $connection->error;
+    $data = array('status' => false, 'message' => $connection->error.message);
+
+    echo json_encode($data);
   }
   
   $connection->close();
